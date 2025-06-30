39f42e4562eceab8c8892999659ec89df6544927
Balaji <3954201+balajin-cse@users.noreply.github.com>
2025-06-30 22:09:45 +0530

    Updated metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,15 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 
-const config = getDefaultConfig(__dirname);
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname, {
+  // [Web-only]: Enables CSS support in Metro.
+  isCSSEnabled: true,
+});
+
+// Add resolution for .web.js extensions to work properly
+config.resolver.sourceExts = process.env.RN_SRC_EXT
+  ? [...process.env.RN_SRC_EXT.split(',').map(ext => ext.trim()), ...config.resolver.sourceExts]
+  : [...config.resolver.sourceExts, 'web.js', 'web.ts', 'web.tsx'];
 
 module.exports = config;
